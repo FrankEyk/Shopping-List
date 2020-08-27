1c7a6060fab0371edb9447f08da4c3d68553ed67
Unknown <frank_eykelhoff@yahoo.de>
2020-08-27 16:00:57 +0200

    Show List of Recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a test', 
+    new Recipe('Test Recipe', 'This is a test',
+    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FexWyqMhrKQY%2Fmaxresdefault.jpg&f=1&nofb=1'),    
+    new Recipe('Test Recipe', 'This is a test',
     'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FexWyqMhrKQY%2Fmaxresdefault.jpg&f=1&nofb=1')
   ];
 
