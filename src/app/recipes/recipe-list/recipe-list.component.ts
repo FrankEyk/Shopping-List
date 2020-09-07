c9c3977b240071495deaa00229d785d3d0d53603
Unknown <frank_eykelhoff@yahoo.de>
2020-09-07 15:09:13 +0200

    Implement recipe-item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,19 +4,23 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a test',
-    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FexWyqMhrKQY%2Fmaxresdefault.jpg&f=1&nofb=1'),    
-    new Recipe('Test Recipe', 'This is a test',
-    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FexWyqMhrKQY%2Fmaxresdefault.jpg&f=1&nofb=1')
+    new Recipe(
+      'Test Recipe',
+      'This is a test',
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FexWyqMhrKQY%2Fmaxresdefault.jpg&f=1&nofb=1'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'This is a test',
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FexWyqMhrKQY%2Fmaxresdefault.jpg&f=1&nofb=1'
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
